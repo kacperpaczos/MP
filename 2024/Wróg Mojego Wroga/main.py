419f69bf16012b6f6675d21f53801ecb805eabc6
Kacper Paczos <kacper-paczos@linux.pl>
2024-02-07 00:29:52 +0100

    LGFM

--- 2024/Wróg Mojego Wroga/main.py
+++ 2024/Wróg Mojego Wroga/main.py
@@ -1,63 +1,79 @@
-import numpy as np
-from collections import defaultdict
+from collections import defaultdict, deque
 
-def bfs(graph, start, closed):
-    visited = np.zeros(len(graph), dtype=bool)
-    visited[start] = True
-    queue = [start]
-    while queue:
-        node = queue.pop(0)
-        for neighbour in graph[node]:
-            if not visited[neighbour] and neighbour not in closed:
-                visited[neighbour] = True
-                queue.append(neighbour)
-    return visited
+class KingdomRelations:
+    def __init__(self, n):
+        self.graph = defaultdict(list)
+        self.closed = set()
+        self.friends = defaultdict(set)
+        self.party_info = defaultdict(int)
+        self.king_status = [0] * n
+        self.n = n
 
-def process_events(n, events, graph):
-    closed = set()
-    party_info = defaultdict(int)
-    king_status = np.zeros(n, dtype=int)
-    parties_known = []
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
 
-    for event in events:
-        action, king = event
+    def update_friends(self):
+        for node in self.graph:
+            for enemy in self.graph[node]:
+                for friend_of_enemy in self.graph[enemy]:
+                    if friend_of_enemy != node:  # Unikamy dodawania samego siebie
+                        self.friends[node].add(friend_of_enemy)
+                        self.friends[friend_of_enemy].add(node)
+
+    def bfs(self, start):
+        visited = [False] * self.n
+        visited[start] = True
+        queue = deque([start])
+        while queue:
+            node = queue.popleft()
+            for neighbour in self.graph[node]:
+                if not visited[neighbour] and neighbour not in self.closed:
+                    visited[neighbour] = True
+                    queue.append(neighbour)
+        return visited
+
+    def process_event(self, action, king):
         king -= 1
-
-        if action in [1, 2]:  # Król wyjeżdża lub wraca
+        if action in [1, 2]:
             if action == 1:
-                closed.add(king)
-            else:
-                closed.remove(king)
-
-        elif action == 3:  # Organizacja imprezy
-            accessible_kingdoms = bfs(graph, king, closed)
-            party_info[king] += 1
-            known_by = 0  # Zmiana: inicjalizacja liczby królów znających imprezę
-            for k in range(len(accessible_kingdoms)):
-                if accessible_kingdoms[k]:  # Usunięcie warunku sprawdzającego, czy król nie jest organizatorem
+                self.closed.add(king)
+            elif action == 2:
+                self.closed.remove(king)
+        elif action == 3:
+            accessible_kingdoms = self.bfs(king)
+            self.party_info[king] += 1
+            known_by = 0
+            for k, is_accessible in enumerate(accessible_kingdoms):
+                if is_accessible and k not in self.closed:
                     known_by += 1
-                    if k != king and k not in closed:  # Dodanie warunku sprawdzającego, czy król nie jest organizatorem
-                        king_status[k] -= 1  # Królowie wiedzący o imprezie, ale nie organizujący, tracą 1 punkt
-            if king not in closed:  # Jeśli król organizator nie jest zamknięty, to zyskuje punkty za każdego gościa
-                king_status[king] += known_by - 1  # Aktualizacja statusu króla - uwzględnienie organizatora imprezy
-            parties_known.append(known_by)
+                    if k in self.friends[king] or k == king:
+                        self.king_status[k] += 1
+                    else:
+                        self.king_status[k] -= 1
+            return known_by
+        return None
 
-    return parties_known, king_status
+    def process_events(self, events):
+        parties_known = []
+        for event in events:
+            known_by = self.process_event(*event)
+            if known_by is not None:
+                parties_known.append(known_by)
+        return parties_known, self.king_status
 
 def main():
     n = int(input())
-    graph = defaultdict(list)
+    relations = KingdomRelations(n)
     for _ in range(n - 1):
         u, v = map(int, input().split())
-        u -= 1
-        v -= 1
-        graph[u].append(v)
-        graph[v].append(u)
+        relations.add_edge(u - 1, v - 1)
 
     q = int(input())
     events = [list(map(int, input().split())) for _ in range(q)]
 
-    parties_known, king_status = process_events(n, events, graph)
+    relations.update_friends()
+    parties_known, king_status = relations.process_events(events)
 
     print(' '.join(map(str, parties_known)))
     print(' '.join(map(str, king_status)))
